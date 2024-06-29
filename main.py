8bd604081bfbd83f541e29e4c0f8476964ae773a
mooghen2s <167309023+mooghen2s@users.noreply.github.com>
2024-06-29 13:57:55 +0700

    [skip ci]

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 import os
 import subprocess
 import argparse
-import requests
 def main() -> None:
-  if os.path.exists('tmp'):
-    os.makedirs('tmp')
   dialog_file = f'output_{number_file}.txt'
   output_dat = f'output_{number_file}.dat'
   audio = f'output_{number_file}.mp3'
   audio_ogg = f'output_{number_file}.ogg'
-  donwload_dialog_file = f"https://raw.githubusercontent.com/mooghen2s/data/top_tier_edge/top_tier_edge/output_{number_file}.txt"
-  download_audio = f"https://raw.githubusercontent.com/mooghen2s/data/top_tier_edge/top_tier_edge/output_{number_file}.mp3"
-  response = requests. get(donwload_dialog_file)
-  if response.status_code == 200:
-    with open(dialog_file, "wb") as file:
-      file. write(response.content)
-  response = requests. get(donwload_audio)
-  if response.status_code == 200:
-    with open(audio, "wb") as file:
-      file. write(response.content)
   command = [
       'ffmpeg', '-y',
       '-loglevel', 'error',
       '-i', audio,
       '-ar', '16000', 
-      audio
+      audio_ogg
   ]
   subprocess.run(command)
   command = [
@@ -40,7 +27,7 @@ def main() -> None:
       audio_ogg
   ]
   subprocess.run(command)
-  os.unlink(audio_ogg)
+  print(f'done for: {audio}')
 
   
 if __name__ == "__main__":
