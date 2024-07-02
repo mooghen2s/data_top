c39c09a67fb19eb176af55107f33d133dd0775fe
mooghen2s <167309023+mooghen2s@users.noreply.github.com>
2024-07-03 00:26:27 +0700

    [skip ci]

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main() -> None:
   dialog_file = f'output_{number_file}.txt'
   OUTPUT_FILE = f'output_{number_file}.mp3'
   with open(dialog_file, 'r', encoding='utf-8') as re_file:
-    TEXT = re_file.readlines()
+    TEXT = re_file.read()
   VOICE = "zh-CN-YunxiaNeural"
   communicate = edge_tts.Communicate(str(TEXT), VOICE, rate="-10%")
   with open(OUTPUT_FILE, "wb") as file:
